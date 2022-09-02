1c6af86d4933c503ee501671f472cb232468f478
AnastasiqDimitrova <109695621+AnastasiqDimitrova@users.noreply.github.com>
2022-09-02 21:55:17 +0300

    Add project files.

---
+++ 01. Numbers Ending in 7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01.NumbersEndingIn7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 7; number <= 997; number += 10)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ 04.CleverLily/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04.CleverLily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double priceForLoundry = double.Parse(Console.ReadLine());
+            int priceForToy = int.Parse(Console.ReadLine());
+
+            int toys = 0;
+            int money = 0;
+            int evenBirthdays = 0;
+            int extraMoney = 0;
+
+            for (int countOfBirthdays = 1; countOfBirthdays <= age; countOfBirthdays++)
+            {
+                if (countOfBirthdays % 2 == 0)
+                {
+                    evenBirthdays++;
+                    extraMoney += 10;
+                    money += extraMoney;
+                }
+                else
+                {
+                    toys++;
+                }
+            }
+            int moneyFromToys = toys * priceForToy;
+            int finalSum = (money + moneyFromToys) - evenBirthdays;
+
+            if (finalSum >= priceForLoundry)
+            {
+                double moneyLeft = finalSum - priceForLoundry;
+                Console.WriteLine($"Yes! {moneyLeft:f2}");
+            }
+            else
+            {
+                double moneyNeeded = priceForLoundry - finalSum;
+                Console.WriteLine($"No! {moneyNeeded:f2}");
+            }
+        }
+    }
+}
